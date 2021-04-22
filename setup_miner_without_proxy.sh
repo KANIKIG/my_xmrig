fff54b6aa44fae7c2e1e957d1b77cabd63ee9b9d
mac <mac@macdeMac-mini.local>
2021-04-22 19:50:35 +0800

    1.5

--- setup_miner_without_proxy.sh
+++ setup_miner_without_proxy.sh
@@ -67,6 +67,55 @@ if [ -z $EXP_MONERO_HASHRATE ]; then
   exit 1
 fi
 
+power2() {
+  if ! type bc >/dev/null; then
+    if   [ "$1" -gt "8192" ]; then
+      echo "8192"
+    elif [ "$1" -gt "4096" ]; then
+      echo "4096"
+    elif [ "$1" -gt "2048" ]; then
+      echo "2048"
+    elif [ "$1" -gt "1024" ]; then
+      echo "1024"
+    elif [ "$1" -gt "512" ]; then
+      echo "512"
+    elif [ "$1" -gt "256" ]; then
+      echo "256"
+    elif [ "$1" -gt "128" ]; then
+      echo "128"
+    elif [ "$1" -gt "64" ]; then
+      echo "64"
+    elif [ "$1" -gt "32" ]; then
+      echo "32"
+    elif [ "$1" -gt "16" ]; then
+      echo "16"
+    elif [ "$1" -gt "8" ]; then
+      echo "8"
+    elif [ "$1" -gt "4" ]; then
+      echo "4"
+    elif [ "$1" -gt "2" ]; then
+      echo "2"
+    else
+      echo "1"
+    fi
+  else 
+    echo "x=l($1)/l(2); scale=0; 2^((x+0.5)/1)" | bc -l;
+  fi
+}
+
+PORT=$(( $EXP_MONERO_HASHRATE * 30 ))
+PORT=$(( $PORT == 0 ? 1 : $PORT ))
+PORT=`power2 $PORT`
+PORT=$(( 10000 + $PORT ))
+if [ -z $PORT ]; then
+  echo "ERROR: Can't compute port"
+  exit 1
+fi
+
+if [ "$PORT" -lt "10001" -o "$PORT" -gt "18192" ]; then
+  echo "ERROR: Wrong computed port value: $PORT"
+  exit 1
+fi
 
 
 # printing intentions
@@ -170,7 +219,7 @@ if [ ! -z $EMAIL ]; then
   PASS="$PASS:$EMAIL"
 fi
 
-sed -i 's/"url": *"[^"]*",/"url": "mine.kanikig.xyz:3333",/' $HOME/moneroocean/config.json
+sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:'$PORT'",/' $HOME/moneroocean/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/moneroocean/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/moneroocean/config.json
 sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/moneroocean/config.json
